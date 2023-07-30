326f544eb7e70c78cd2a00a5b34711bb55728fc1
Rishabh More <rushabh.more@wednesday.is>
2023-07-31 00:09:10 +0530

    Add ARCore checks before app initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart'
+    show ArCoreController;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  debugPrint('ARCORE IS AVAILABLE? ');
+  debugPrint(await ArCoreController.checkArCoreAvailability());
+  debugPrint('\nAR SERVICES INSTALLED? ');
+  debugPrint(await ArCoreController.checkIsArCoreInstalled());
   runApp(const MyApp());
 }
 
